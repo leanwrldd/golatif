20fc54d6a63f0d69c75ff034d759013437366a7c
golatif <leanwrldd@gmail.com>
2024-12-19 10:05:47 +0300

    finally works

--- pages/api/validateuser.js
+++ pages/api/validateuser.js
@@ -2,17 +2,33 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  const userCredsFilepath = path.join(process.cwd(),'public' ,'database', 'usercreds.json');
-    if (req.method !== "POST") {
-      return res.status(405).json({ message: "Method not allowed" });
-    }
+  const userCredsFilepath = path.join(process.cwd(), 'public', 'database', 'usercreds.json');
+
+  if (req.method !== "POST") {
+    console.log('Method not allowed');
+    return res.status(405).json({ message: "Method not allowed" });
+  }
+  const { username, password } = req.body;
   fs.readFile(userCredsFilepath, 'utf8', (err, data) => {
     if (err) {
+      console.log('Error reading user data:', err);
       return res.status(500).json({ message: "An error occurred while reading user data" });
     }
-    else {
-        console.log('User Data:', data);
-        return res.status(200).json({ users: JSON.parse(data) });
+
+    try {
+      const users = JSON.parse(data);
+      const user = users.find(user => user.username === username && user.password === password);
+
+      if (user) {
+        console.log('Login successful');
+        return res.status(200).json({ message: "Login successful" });
+      } else {
+        console.log('Invalid credentials');
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+    } catch (parseError) {
+      console.error('Error parsing user data:', parseError);
+      return res.status(500).json({ message: "An error occurred while parsing user data" });
     }
   });
 }
