031de957c51092db7e389e7f7b9f6ee7c313b4c9
golatif <leanwrldd@gmail.com>
2024-12-19 09:58:24 +0300

    Update validateuser.js

--- app/api/validateuser.js
+++ app/api/validateuser.js
@@ -8,6 +8,8 @@ export default function handler(req, res) {
     return res.status(405).json({ message: "Method not allowed" });
   }
 
+  const { username, password } = req.body;
+
   fs.readFile(userCredsFilepath, 'utf8', (err, data) => {
     if (err) {
       return res.status(500).json({ message: "An error occurred while reading user data" });
@@ -15,15 +17,13 @@ export default function handler(req, res) {
 
     try {
       const users = JSON.parse(data);
-      const { username, password } = req.body;
 
       console.log('Request Body:', req.body); // Debugging statement
-      console.log('Parsed Users:', users); // Debugging statement
 
       const user = users.find(user => user.username === username && user.password === password);
 
       if (user) {
-        return res.status(200).json({ message: "Login successful" });
+        return res.status(201).json({ message: "Login successful" });
       } else {
         return res.status(401).json({ message: "Invalid credentials" });
       }
