8f2a9abe79fa2c6c9c779269653732792cdebca3
golatif <leanwrldd@gmail.com>
2024-12-19 09:59:22 +0300

    Update

--- app/api/validateuser.js
+++ app/api/validateuser.js
@@ -5,13 +5,17 @@ export default function handler(req, res) {
   const userCredsFilepath = path.join(process.cwd(), 'public', 'database', 'usercreds.json');
   
   if (req.method !== "POST") {
+    console.log('Method not allowed');
     return res.status(405).json({ message: "Method not allowed" });
   }
 
   const { username, password } = req.body;
+  console.log('Received username:', username);
+  console.log('Received password:', password);
 
   fs.readFile(userCredsFilepath, 'utf8', (err, data) => {
     if (err) {
+      console.log('Error reading user data:', err);
       return res.status(500).json({ message: "An error occurred while reading user data" });
     }
 
@@ -19,12 +23,15 @@ export default function handler(req, res) {
       const users = JSON.parse(data);
 
       console.log('Request Body:', req.body); // Debugging statement
+      console.log('Parsed Users:', users); // Debugging statement
 
       const user = users.find(user => user.username === username && user.password === password);
 
       if (user) {
-        return res.status(201).json({ message: "Login successful" });
+        console.log('Login successful');
+        return res.status(200).json({ message: "Login successful" });
       } else {
+        console.log('Invalid credentials');
         return res.status(401).json({ message: "Invalid credentials" });
       }
     } catch (parseError) {
